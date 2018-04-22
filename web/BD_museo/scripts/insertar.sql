96c2773978066d9f1724d179c7ed6f102d8525e9
Algebro <AlgebroHeavyGuns@users.noreply.github.com>
2018-04-22 14:32:57 +0200

    Rename insertar.txt to insertar.sql

--- web/BD_museo/scripts/insertar.sql
+++ web/BD_museo/scripts/insertar.sql
@@ -5,21 +5,21 @@ INSERT INTO	Obra
   VALUES (
     '1513',
     "<p>
-      <b>'La Duquesa Fea'</b> o tambiÈn conocida como '<b>Una mujer mayor</b>',
+      <b>'La Duquesa Fea'</b> o tambi√©n conocida como '<b>Una mujer mayor</b>',
       es un cuadro del pintor flamenco <b>Quentin Massys</b>, realizado
-      cerca de 1513, al Ûleo.
+      cerca de 1513, al √≥leo.
     </p>
     <p>
-      Es un retrato satÌrico de una mujer aristÛcrata. De una forma
+      Es un retrato sat√≠rico de una mujer arist√≥crata. De una forma
       inapropiada trata de recrear su juventud con un atuendo fuera de
-      su edad. Las malformaciones en la mujer se deben a que sufrÌa de
-      la enfermedad de Paget que ocasiona una destrucciÛn y regeneraciÛn
-      Ûsea anormal (deforme) en los huesos.
+      su edad. Las malformaciones en la mujer se deben a que sufr√≠a de
+      la enfermedad de Paget que ocasiona una destrucci√≥n y regeneraci√≥n
+      √≥sea anormal (deforme) en los huesos.
     </p>
     <p>
       La obra fue donada en 1947 por Jenny Louisa Roberta Blaker a la
       National Gallery de Londres en Inglaterra, donde se encuentra en
-      exhibiciÛn permanente.
+      exhibici√≥n permanente.
     </p>",
     "La Duquesa Fea",
     "Quentin Massys",
@@ -30,10 +30,10 @@ INSERT INTO	Obra
 (
   '2011',
   "<p>
-    Detr·s de todas las cosas disfrazadas por la moral y los prejuicios
+    Detr√°s de todas las cosas disfrazadas por la moral y los prejuicios
     de la sociedad, existe un lado aberrante que busca ser expuesto
     para contradecir los estatutos de la belleza y cuestionar sus
-    valores estÈticos. El lado terrible de la vida es el enfoque
+    valores est√©ticos. El lado terrible de la vida es el enfoque
     principal de la obra de los hermanos Jake y Dinos Chapman, quienes
     buscan crear arte a partir de lo grotesco y lo profano.
   </p>",
@@ -47,15 +47,15 @@ INSERT INTO	Obra
   '1819',
   "<p>
     El cuadro Saturno devorando a su hijo o Saturno devorando a un hijo es 
-    una de las pinturas al Ûleo sobre revoco que formaron parte de la decoraciÛn 
-    de los muros de la casa que Francisco de Goya adquiriÛ en 1819, llamada la Quinta del Sordo. 
+    una de las pinturas al √≥leo sobre revoco que formaron parte de la decoraci√≥n 
+    de los muros de la casa que Francisco de Goya adquiri√≥ en 1819, llamada la Quinta del Sordo. 
     Por tanto, la obra pertenece a la serie de las Pinturas negras de dicho artista.
   </p>
   <p>
-    Representa al tit·n Crono, como es habitual indiferenciado de Chronos, o Saturno en 
-    la mitologÌa romana, en el acto de devorar a uno de sus hijos. 
-    La figura era emblema alegÛrico del paso del tiempo, pues Crono se comÌa a los
-    hijos reciÈn nacidos de Rea, su mujer, por temor a ser destronado por uno de ellos.
+    Representa al tit√°n Crono, como es habitual indiferenciado de Chronos, o Saturno en 
+    la mitolog√≠a romana, en el acto de devorar a uno de sus hijos. 
+    La figura era emblema aleg√≥rico del paso del tiempo, pues Crono se com√≠a a los
+    hijos reci√©n nacidos de Rea, su mujer, por temor a ser destronado por uno de ellos.
   </p>",
   "Saturno devorando a su hijo",
   "Francisco de Goya",
@@ -67,12 +67,12 @@ INSERT INTO	Obra
   '2012',
   "<p>
     Las pinturas de Rex van Minnen poseen un marcado estilo renacentista, 
-    con un toque casi poÈtico de carnosas ampollas purulentas, grotescas, 
-    deformadas, desfiguradas y en descomposiciÛn.
-    Van Minnen parece empujar los lÌmites de la estÈtica visual y psicolÛgica. 
+    con un toque casi po√©tico de carnosas ampollas purulentas, grotescas, 
+    deformadas, desfiguradas y en descomposici√≥n.
+    Van Minnen parece empujar los l√≠mites de la est√©tica visual y psicol√≥gica. 
     Los retratos desfigurados arraigados en la actualidad, con tatuajes y cortes de pelo, 
     sugieren un sentido de realismo horripilante. 
-    Ni las naturalezas muertas est·n seguras cuando son tocadas por la mano de van Minnen.
+    Ni las naturalezas muertas est√°n seguras cuando son tocadas por la mano de van Minnen.
   </p>",
   "Piggy Boy",
   "Christian Rex Van Minnen",
