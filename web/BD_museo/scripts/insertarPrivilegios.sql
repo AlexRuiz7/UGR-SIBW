b9ca44f5c78d4f5f6fa51c33d8db3c101fada452
Algebro <AlgebroHeavyGuns@users.noreply.github.com>
2018-05-11 17:04:28 +0200

    Create insertarPrivilegios.sql

---
+++ web/BD_museo/scripts/insertarPrivilegios.sql
@@ -0,0 +1,14 @@
+INSERT INTO        nivelprivilegios
+(titulo, editardatos, comentar, moderar, gestionmuseo, gestionpermisos)
+
+ VALUES
+
+("Anónimo", 0, 0, 0, 0, 0),
+
+("Registrado", 1, 1, 0, 0, 0),
+
+("Moderador", 1, 1, 1, 0, 0),
+
+("Gestor", 1, 1, 1, 1, 0),
+
+("Superusuario", 1, 1, 1, 1, 1);
