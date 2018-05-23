84657ad2d1d120e5105d630839e92005b5a80d60
Algebro <AlgebroHeavyGuns@users.noreply.github.com>
2018-05-23 09:08:26 +0200

    Add files via upload

---
+++ web/crearBD_root.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+
+
+mysql -u root < crear.sql
+
+mysql -u root museo < insertar_obras.sql
+mysql -u root museo < insertar_comentarios.sql
+
+mysql -u root museo < insertar_colecciones.sql
+
+mysql -u root museo < insertar_datos_web.sql
+
+mysql -u root museo < insertar_palabras_mal.sql
